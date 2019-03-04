6c395f3d657bf7e6fe9721fc793691967785676e
josharmantrout91 <josh.armantrout@gmail.com>
2019-03-04 18:06:27 -0500

    Successful POST to log in

--- index.js
+++ index.js
@@ -40,6 +40,23 @@ server.post("/api/register", (req, res) => {
 
 // POST to log in
 
+server.post("/api/login", (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome, ${user.username}.` });
+      } else {
+        res.status(401).json({ error: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
 // GET once user is authN/authZ to show list of users
 
 const port = process.env.PORT || 5500;
