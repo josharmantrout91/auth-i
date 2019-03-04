6f89e332e5344e1dd0c1a2baa6c3260012833102
josharmantrout91 <josh.armantrout@gmail.com>
2019-03-04 17:29:50 -0500

    Successful POST for user registry

--- index.js
+++ index.js
@@ -16,5 +16,31 @@ server.get("/", (req, res) => {
   res.send("**Taps Mic** Is this thing on??");
 });
 
+// POST to register new user
+
+server.post("/api/register", (req, res) => {
+  const user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 10);
+
+  user.password = hash;
+
+  Users.add(user)
+    .then(savedUser => {
+      if (savedUser) {
+        res.status(201).json(savedUser);
+      } else {
+        res.status(404).json({ error: "No user registered" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+// POST to log in
+
+// GET once user is authN/authZ to show list of users
+
 const port = process.env.PORT || 5500;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
